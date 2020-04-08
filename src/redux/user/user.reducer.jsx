ef43bb367247be8e136fafad57be059e99cdff44
amangspathak <akp9977@gmail.com>
2020-04-08 18:56:24 +0530

    Implement Redux Create Reducer component Create Action to connent with root component

--- src/redux/user/user.reducer.jsx
+++ src/redux/user/user.reducer.jsx
@@ -4,15 +4,15 @@ const INITIAL_STATE = {
 
 const userReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
-        case '':
+        case 'SET_CURRENT_USER':
            return {
                 ...state,
                 currentUser: action.payload
-            }
+            };
         default:
             return state;
     }
-}
+};
 
 export default userReducer;
 
